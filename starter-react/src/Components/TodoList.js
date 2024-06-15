613a7d865ceded1d6634ae1765bbeeaa4c0b41be
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2024-06-15 12:12:05 +0200

    simple React component finished

--- starter-react/src/Components/TodoList.js
+++ starter-react/src/Components/TodoList.js
@@ -1,32 +1,40 @@
 import Button from "@mui/material/Button";
 import React from 'react'
 function ToDoList (){
-    const [list, setList] = React.useState(["test","testje"]);
+    const [list, setList] = React.useState([{text: "test", completed: false},{text: "testje", completed: false}]);
+    const [inputItem,setInputItem] = React.useState("");
+
     const buttonClick = () => {
-        setList(list => [...list, "test"])
+        setList(list => [...list, {text: inputItem, completed: false}])
+        setInputItem("");
     }
 
-    const deleteItem = (index) => {
-        const copy = [...list]
-        copy.splice(index,1)
-        setList(copy)
-    }
+    // const deleteItem = (index) => {
+    //     const copy = [...list]
+    //     copy.splice(index,1)
+    //     setList(copy)
+    // }
 
     const deleteItemAdvanced = (index) => {
         setList(list.filter((item,i)=>index !== i));
+    }
+
+    const changeCompleted = (index) => {
+        list[index].completed = !list[index].completed
     }
 
 
     return(
         <div>
             <label htmlFor={"ListItem"}>To do item</label>
-            <input id={"ListItem"}/>
+            <input id={"ListItem"} value={inputItem} onChange={e => setInputItem(e.target.value)}/>
             <Button variant={"contained"} onClick={()=>buttonClick()}>Add Item</Button>
 
             <ul>
                 {list.map((listItem,index)=>(
                     <div>
-                        <li>{listItem}</li>
+                        <input type={"checkbox"} value={listItem.completed} onChange={()=>changeCompleted(index)}/>
+                        <li>{listItem.text}</li>
                         <Button onClick={()=>deleteItemAdvanced(index)}>X</Button>
                     </div>
                 ))}
